65a27ca5922a2d3861019a12fb7903c7180f5540
Niall Eccles <nialle@rothco.ie>
2022-08-22 19:43:15 +0100

    Added in logo.

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,13 +1,17 @@
 import { Button, Checkbox, Navbar } from "flowbite-react";
 import type { NextComponentType, NextPage } from "next";
 import Head from "next/head";
+import NextpassLogo from "./nextpass-logo";
 
 const Nav: NextComponentType = () => {
   return (
     <Navbar fluid={true} rounded={true}>
+      <div className="flex flex-row items-end">
+        <NextpassLogo />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white text-center">
           Nextpass
         </span>
+      </div>
     </Navbar>
   );
 };
